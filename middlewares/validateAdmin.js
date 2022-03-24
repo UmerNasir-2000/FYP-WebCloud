1498c3d2c159740ed1a0910e7c137cd2f6e6e452
Umer Nasir <umer.nas@outlook.com>
2022-03-24 18:03:36 +0500

    Added Admin Middleware Functionality

--- middlewares/validateAdmin.js
+++ middlewares/validateAdmin.js
@@ -1,3 +1,21 @@
-const isAdmin = (req, res, next) => {};
+const { StatusCodes } = require("http-status-codes");
+const { users } = require("../models");
 
-module.exports = isAdmin;
+const validateAdmin = async (req, res, next) => {
+  const isAdminUser = await users.findOne({
+    where: {
+      id: req.user.id,
+      is_admin: true,
+    },
+  });
+
+  if (isAdminUser == null) {
+    res
+      .status(StatusCodes.UNAUTHORIZED)
+      .json({ message: "Only Admin User Can Perform This Action" });
+  }
+
+  next();
+};
+
+module.exports = validateAdmin;
--- routes/project.route.js
+++ routes/project.route.js
@@ -2,13 +2,14 @@ const express = require("express");
 const router = express.Router();
 const validateResource = require("../middlewares/validateRequest");
 const validateToken = require("../middlewares/validateToken");
+const validateAdmin = require("../middlewares/validateAdmin");
 const createProjectSchema = require("../schemas/project/create-project-template.schema");
 const {
   createUserProjectTemplate,
   getUserProjects,
 } = require("../controllers/project.controller");
 
-router.route("/user").get(validateToken, getUserProjects);
+router.route("/user").get(validateToken, validateAdmin, getUserProjects);
 
 router
   .route("/create-template")
