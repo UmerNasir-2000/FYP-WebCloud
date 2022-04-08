f9aa199be546083dbb61c1e8dc586e6aaa11e7ee
Umer Nasir <umer.nas@outlook.com>
2022-04-08 15:30:39 +0500

    Integrated Admin Repositories API

---
+++ public/admin_view_repo.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  $.ajax({
+    method: "GET",
+    url: "/api/repo/",
+    beforeSend: function (xhr) {
+      xhr.setRequestHeader(
+        "Authorization",
+        `Bearer ${localStorage.getItem("token")}`
+      );
+    },
+    success: function (result, status, xhr) {
+      console.log(result);
+    },
+    error: function (xhr, status, error) {
+      alert(xhr.responseText);
+    },
+  });
+});
