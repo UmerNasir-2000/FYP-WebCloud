dfca0bc9582e3c89188c6e8a2a4c9eb30623957d
Umer Nasir <umer.nas@outlook.com>
2022-04-08 23:11:15 +0500

    Integrated like repository API

--- public/detailrepo.js
+++ public/detailrepo.js
@@ -1,6 +1,24 @@
 $(document).ready(function () {
   /**${localStorage.getItem("publicRepoId")} */
-
+  /**${localStorage.getItem("currentProjectId")} */
+  $("#likeBtn").click(function () {
+    $.ajax({
+      method: "POST",
+      url: `/api/repo/like/1`,
+      beforeSend: function (xhr) {
+        xhr.setRequestHeader(
+          "Authorization",
+          `Bearer ${localStorage.getItem("token")}`
+        );
+      },
+      success: function (result, status, xhr) {
+        console.log(result);
+      },
+      error: function (xhr, status, error) {
+        alert(xhr.responseText);
+      },
+    });
+  });
   $("#forkBtn").click(function () {
     $.ajax({
       method: "POST",
