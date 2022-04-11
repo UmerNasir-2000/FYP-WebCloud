0529f7c8e41ad73dd4ae816f1e22fb03e55cdaf9
Umer Nasir <umer.nas@outlook.com>
2022-04-11 10:55:22 +0500

    Fixed Xterm

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const pty = require("node-pty");
+const os = require("os");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
@@ -9,25 +10,77 @@ const app = express();
 const expressWs = require("express-ws")(app);
 const logger = require("./utils/logger");
 
+const USE_BINARY = os.platform() !== "win32";
+
 dotenv.config();
 
-var terminals = {};
+var terminals = {},
+  logs = {};
 
 app.use(express.static(__dirname + "/public"));
 
 expressWs.app.ws("/terminals/:pid", function (ws, req) {
   var term = terminals[parseInt(req.params.pid)];
+  console.log("Connected to terminal " + term.pid);
+  ws.send(logs[term.pid]);
+
+  // string message buffering
+  function buffer(socket, timeout) {
+    let s = "";
+    let sender = null;
+    return (data) => {
+      s += data;
+      if (!sender) {
+        sender = setTimeout(() => {
+          socket.send(s);
+          s = "";
+          sender = null;
+        }, timeout);
+      }
+    };
+  }
+  // binary message buffering
+  function bufferUtf8(socket, timeout) {
+    let buffer = [];
+    let sender = null;
+    let length = 0;
+    return (data) => {
+      buffer.push(data);
+      length += data.length;
+      if (!sender) {
+        sender = setTimeout(() => {
+          socket.send(Buffer.concat(buffer, length));
+          buffer = [];
+          sender = null;
+          length = 0;
+        }, timeout);
+      }
+    };
+  }
+  const send = USE_BINARY ? bufferUtf8(ws, 5) : buffer(ws, 5);
 
   term.on("data", function (data) {
-    ws.send(data);
+    try {
+      send(data);
+    } catch (ex) {
+      // The WebSocket is not open, ignore
+    }
   });
-
   ws.on("message", function (msg) {
     term.write(msg);
+  });
+  ws.on("close", function () {
+    term.kill();
+    console.log("Closed terminal " + term.pid);
+    // Clean things up
+    delete terminals[term.pid];
+    delete logs[term.pid];
   });
 });
 
 app.post("/terminals", function (req, res) {
+  const env = Object.assign({}, process.env);
+  env["COLORTERM"] = "truecolor";
   var cols = parseInt(req.query.cols),
     rows = parseInt(req.query.rows),
     term = pty.spawn(process.platform === "win32" ? "cmd.exe" : "bash", [], {
@@ -41,7 +94,10 @@ app.post("/terminals", function (req, res) {
 
   console.log("Created terminal with PID: " + term.pid);
   terminals[term.pid] = term;
-
+  logs[term.pid] = "";
+  term.on("data", function (data) {
+    logs[term.pid] += data;
+  });
   res.send(term.pid.toString());
   res.end();
 });
