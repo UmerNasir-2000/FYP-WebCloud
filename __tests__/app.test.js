90cd4166640fe8f6140b79bfb093325e3112c9e6
Umer Nasir <umer.nas@outlook.com>
2022-04-11 12:00:57 +0500

    Added chai, chai-http & mocha for unit tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,5 +1,8 @@
 const app = require("../server");
 
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+
 describe("APPLICATION SHOULD START", () => {
   it("SHOULD RUN", () => {});
 });
