edcf22713842e6297b9e746452c4a6047dd3f49f
Umer Nasir <umer.nas@outlook.com>
2022-04-26 19:34:01 +0500

    Added Swagger Docs On Admin Routes

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -22,7 +22,7 @@ const logDatabase = require("../middlewares/logDatabase");
  *  get:
  *     tags:
  *     - Admin
- *     summary: Dashboard API For Admin
+ *     summary: Dashboard API For Admin ; Can Be Performed By Admin Only
  *     responses:
  *      200:
  *        description: Success
@@ -72,13 +72,14 @@ const logDatabase = require("../middlewares/logDatabase");
 router
   .route("/dashboard")
   .get(validateToken, validateAdmin, logDatabase, dashboard);
+
 /**
  * @openapi
  * '/api/admin/charts':
  *  post:
  *     tags:
  *     - Admin
- *     summary: Fetch Charts Based On The Entity Value Provided
+ *     summary: Fetch Charts Based On The Entity Value Provided ; Can Be Performed By Admin Only
  *     requestBody:
  *      required: true
  *      content:
@@ -122,14 +123,131 @@ router
     charts
   );
 
+/**
+ * @openapi
+ * '/api/admin/requests':
+ *  get:
+ *     tags:
+ *     - Admin
+ *     summary: Fetch All Requests For Admin ; Can Be Performed By Admin Only
+ *     responses:
+ *      200:
+ *        description: Success
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                requests:
+ *                  type: array
+ *                  items:
+ *                    type: object
+ *                    properties:
+ *                      id:
+ *                       type: number
+ *                      status:
+ *                       type: string
+ *                      email:
+ *                       type: string
+ *                      first_name:
+ *                       type: string
+ *                      last_name:
+ *                        type: string
+ *                      createdAt:
+ *                        type: string
+ *                        format: date
+ *                      is_public:
+ *                        type: boolean
+ *                      project_name:
+ *                        type: string
+ *                      description:
+ *                        type: string
+ *      400:
+ *        description: Bad request
+ */
 router
   .route("/requests")
   .get(validateToken, validateAdmin, logDatabase, getRequests);
 
+/**
+ * @openapi
+ * '/api/admin/dashboard-requests':
+ *  get:
+ *     tags:
+ *     - Admin
+ *     summary: Fetch All Requests For Admin Dashboard ; Can Be Performed By Admin Only
+ *     responses:
+ *      200:
+ *        description: Success
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                requests:
+ *                  type: array
+ *                  items:
+ *                    type: object
+ *                    properties:
+ *                      id:
+ *                       type: number
+ *                      status:
+ *                       type: string
+ *                      email:
+ *                       type: string
+ *                      first_name:
+ *                       type: string
+ *                      last_name:
+ *                        type: string
+ *                      createdAt:
+ *                        type: string
+ *                        format: date
+ *                      is_public:
+ *                        type: boolean
+ *                      project_name:
+ *                        type: string
+ *                      description:
+ *                        type: string
+ *      400:
+ *        description: Bad request
+ */
 router
   .route("/dashboard-requests")
   .get(validateToken, validateAdmin, logDatabase, dashboardRequests);
 
+/**
+ * @openapi
+ * '/api/admin/user-status/{id}':
+ *  put:
+ *     tags:
+ *     - Admin
+ *     summary: Update User Status With Id ; Can Be Performed By Admin Only
+ *     responses:
+ *      200:
+ *        description: Success
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                user:
+ *                  type: object
+ *                  properties:
+ *                    id:
+ *                     type: number
+ *                    email:
+ *                     type: string
+ *                    is_public:
+ *                     type: boolean
+ *      400:
+ *        description: Bad request
+ */
 router
   .route("/user-status/:id")
   .put(
@@ -140,6 +258,46 @@ router
     updateUserStatus
   );
 
+/**
+ * @openapi
+ * '/api/admin/request/{id}':
+ *  put:
+ *     tags:
+ *     - Admin
+ *     requestBody:
+ *     required: true
+ *     content:
+ *       application/json:
+ *        schema:
+ *          type: object
+ *          required:
+ *            - status
+ *          properties:
+ *            status:
+ *             type: string
+ *     summary: Update Request Status With Id ; Can Be Performed By Admin Only
+ *     responses:
+ *      200:
+ *        description: Success
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                user:
+ *                  type: object
+ *                  properties:
+ *                    id:
+ *                     type: number
+ *                    email:
+ *                     type: string
+ *                    is_public:
+ *                     type: boolean
+ *      400:
+ *        description: Bad request
+ */
 router
   .route("/request/:id")
   .put(
