577689dd1d678d46e65e9ea9866ce9496fd695a7
Umer Nasir <umer.nas@outlook.com>
2022-05-09 19:28:37 +0500

    Resolved Login API Issue

--- services/auth.service.js
+++ services/auth.service.js
@@ -1,5 +1,5 @@
 const bcrypt = require("bcryptjs");
-const argon2 = require("argon2");
+const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
 const { StatusCodes } = require("http-status-codes");
 const asyncHandler = require("express-async-handler");
@@ -18,11 +18,12 @@ const registerUserService = asyncHandler(async (req, res) => {
       .json({ error: "Email Already Exists." });
   }
 
-  // const salt = await bcrypt.genSalt(10);
-  // const hash = await bcrypt.hash(password, salt);
-
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(password, salt);
+
+  // const hash = crypto.createHash("sha512").update(password).digest("hex");
+
+  // console.log("hash :>> ", hash);
 
   const user = await users.create({
     first_name: firstName,
@@ -67,12 +68,17 @@ const loginUserService = asyncHandler(async (req, res) => {
       .status(StatusCodes.BAD_REQUEST)
       .json({ message: "User does not exist" });
 
+  console.log("password :>> ", password);
+
   const correctPassword = await bcrypt.compare(password, validUser.password);
 
-  //const correctPassword = await argon2.verify(validUser.password, password);
-
-  console.log("validUser.password", validUser.password);
   console.log("correctPassword :>> ", correctPassword);
+  // console.log("password :>> ", password);
+  // const hash = crypto.createHash("sha512").update(password).digest("hex");
+  // const correctPassword = validUser.password === hash;
+  // console.log("validUser.password", validUser.password);
+  // console.log("hash :>> ", hash);
+  // console.log("correctPassword :>> ", correctPassword);
 
   if (correctPassword) {
     const accessToken = generateToken(validUser.id);
